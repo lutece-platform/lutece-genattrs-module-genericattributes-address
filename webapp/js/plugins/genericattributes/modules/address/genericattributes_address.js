0a574bd76b74fc9cf6029ecad82669186ad440f1
HOHL <laurent.hohl@gmail.com>
2021-11-25 16:34:54 +0100

    ADDRESSAUTOCOMPLETE-16 : Remove unused button

--- webapp/js/plugins/genericattributes/modules/address/genericattributes_address.js
+++ webapp/js/plugins/genericattributes/modules/address/genericattributes_address.js
@@ -3,9 +3,7 @@
  * @param id_entry entry id
  * @return void
  */
-function checkAddress( id_entry )
-{
-	document.getElementById( id_entry + '_button_check' ).disabled = 'disabled';
+ function checkAddress( id_entry ){
 	var div_results = document.getElementById(id_entry + "_address_results");
 	div_results.innerHTML = '';
 	var addressValue = document.getElementById( id_entry + '_address').value;
@@ -13,9 +11,7 @@ function checkAddress( id_entry )
 	document.getElementById(id_entry + "_x").value = 0;
 	document.getElementById(id_entry + "_y").value = 0;
 	document.getElementById(id_entry + "_idAddress").value = 0;
-	
-	if ( addressValue != null && addressValue != '' )
-	{
+	if ( addressValue != null && addressValue != '' ){
 		$.getJSON(
 				document.getElementsByTagName('base')[0].href + "jsp/site/plugins/genericattributes/modules/address/SearchAddress.jsp",
 				{
@@ -24,70 +20,45 @@ function checkAddress( id_entry )
 				},
 				function ( data )
 				{
-					if ( data.error )
-					{
+					if ( data.error )					{
 						displayError( data.message, id_entry );
-					}
-					else
-					{
+					} else {
 						displayResult( data, id_entry );
 					}
 				}
 		);
 	}
-	else
-	{
-		document.getElementById( id_entry + '_button_check' ).disabled = '';
-	}
 }
 
-function displayResult( data, id_entry )
-{
-	var div_results = document.getElementById(id_entry + "_address_results");
-	
-	if ( data.singleResult )
-	{
-		document.getElementById(id_entry + "_address").value = data.addressLabel;
-		document.getElementById(id_entry + "_x").value = data.x;
-		document.getElementById(id_entry + "_y").value = data.y;
-		document.getElementById(id_entry + "_idAddress").value = data.idAddress;
-	}
-	
-	$('#'+id_entry + "_address_results").html(data.html);
-	document.getElementById( id_entry + '_button_check' ).disabled = '';
+function displayResult( data, id_entry ){
+var div_results = document.getElementById(id_entry + "_address_results");
+if ( data.singleResult ){
+	document.getElementById(id_entry + "_address").value = data.addressLabel;
+	document.getElementById(id_entry + "_x").value = data.x;
+	document.getElementById(id_entry + "_y").value = data.y;
+	document.getElementById(id_entry + "_idAddress").value = data.idAddress;
+}	
+$('#'+id_entry + "_address_results").html(data.html);
 }
 
-function displayError( message, id_entry )
-{
+function displayError( message, id_entry ){
 	$('#'+id_entry + "_address_results").html(message);
-	document.getElementById( id_entry + '_button_check' ).disabled = '';
 }
-
 /**
  * Put the geolocation
  * @param address the address
  * @return void
  */
-function selectAddress( id_entry, code_address )
-{
-	var div_results = document.getElementById(id_entry + "_address_results");
-	div_results.innerHTML = '';
-	$.getJSON(
-			document.getElementsByTagName('base')[0].href + "jsp/site/plugins/genericattributes/modules/address/FindAddressInfos.jsp",
-			{
-				 "id_address" : code_address,
-				 "id_entry" : id_entry
-			},
-			function ( data )
-			{
-				if ( data.error )
-				{
-					displayError( data.message, id_entry );
-				}
-				else
-				{
-					displayResult( data, id_entry );
-				}
-			}
-	);
+function selectAddress( id_entry, code_address ){
+var div_results = document.getElementById(id_entry + "_address_results");
+div_results.innerHTML = '';
+$.getJSON( document.getElementsByTagName('base')[0].href + "jsp/site/plugins/genericattributes/modules/address/FindAddressInfos.jsp",
+	{ "id_address" : code_address,	"id_entry" : id_entry },
+	function ( data ){
+		if ( data.error ){
+			displayError( data.message, id_entry );
+		} else {
+			displayResult( data, id_entry );
+		}
+	});
 }
